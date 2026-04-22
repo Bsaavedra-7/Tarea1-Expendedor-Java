ac76ce93a17562014307a3d045d4a42a68d7e4b8
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-04-21 21:38:03 -0400

    Creado constructor y método para llenar depósitos al inicializar el expendedor

--- src/Expendedor/Expendedor.java
+++ src/Expendedor/Expendedor.java
@@ -6,4 +6,25 @@ import Expendedor.excepciones.*;
 import Expendedor.deposito.*;
 
 public class Expendedor {
+    private int cantProducto; //Variable a utilizar para rellenar los depósitos
+    private Deposito<Producto> dCocaCola;
+    private Deposito<Producto> dFanta;  //Declaramos los depósitos
+    private Deposito<Producto> dSprite;
+
+    public Expendedor(int cantProducto){
+        this.cantProducto = cantProducto;
+        dCocaCola = new Deposito<>();
+        dFanta = new Deposito<>();  //Instanciamos los depósitos de bebidas
+        dSprite = new Deposito<>();
+        llenarDepositos(); //Llamamos al método para que llene los depósitos al inicializarse el expendedor
+    }
+
+    private void llenarDepositos(){
+        for(int i = 0; i < cantProducto; i++){ //Llenamos cada depósito cantProducto veces
+            dCocaCola.add(new CocaCola());
+            dFanta.add(new Fanta());
+            dSprite.add(new Sprite());
+        }
+    }
+
 }

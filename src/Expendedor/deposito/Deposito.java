ff5aad4444e0de9f2d1811d76ed1ebadb3e9fbf2
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-04-21 21:03:29 -0400

    Añadidos métodos principales para el depósito, tanto constructor como getter y método para rellenar

--- src/Expendedor/deposito/Deposito.java
+++ src/Expendedor/deposito/Deposito.java
@@ -1,4 +1,20 @@
 package Expendedor.deposito;
+import java.util.ArrayList;
 
-public class Deposito {
+public class Deposito<T> {
+    private ArrayList<T> array; //Creamos el array genérico
+
+    public Deposito(){  //Constructor del arreglo
+        this.array = new ArrayList<>();
+    }
+
+    public void add(T tipo){ //Método para añadir una vez un objeto al arreglo
+        array.add(tipo);
+    }
+
+    public T get(){ //Método para retirar el objeto
+        if (array.size() == 0){return null;} //Podemos aprovechar esto para hacer el throw a la excepción en expendedor
+        else{return array.removeFirst();} //Quita el primer elemento
+    }
+
 }

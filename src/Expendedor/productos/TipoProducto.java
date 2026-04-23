39c6bccd1cb31ca74b1e2544f3ea290033a1e0fa
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-04-22 20:00:25 -0400

    Añadido atributo número para cada producto, además de getter para este

--- src/Expendedor/productos/TipoProducto.java
+++ src/Expendedor/productos/TipoProducto.java
@@ -1,16 +1,22 @@
 package Expendedor.productos;
 
 public enum TipoProducto{
-    COCA_COLA(1500),
-    FANTA(1500),
-    SPRITE(1500), //Definimos las constantes
-    SNICKERS(1000),
-    SUPER8(500);
+    COCA_COLA(1, 1500),
+    FANTA(2, 1500),
+    SPRITE(3, 1500), //Definimos las constantes
+    SNICKERS(4, 1000),
+    SUPER8(5, 500);
 
+    private final int numero; //Definimos el atributo numero para saber a que numero corresponde cada producto
     private final int precio; //Definimos el atributo precio
 
-    TipoProducto(int precio){
+    TipoProducto(int numero, int precio){
+        this.numero = numero;
         this.precio = precio; //Método constructor
+    }
+
+    public int getNumero(){
+        return numero;
     }
 
     public int getPrecio() {

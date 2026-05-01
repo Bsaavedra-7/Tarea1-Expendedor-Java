5c8479760cb46a97f508d8dc041511ab77304792
mguerrerozam <mguerrero2025@inf.udec.cl>
2026-05-01 17:56:11 -0400

    feat: Añadida comprobación del sort de monedas

--- src/Expendedor/Main.java
+++ src/Expendedor/Main.java
@@ -1,7 +1,20 @@
 package Expendedor;
+import Expendedor.monedas.*;
 
+import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
+
         System.out.println("Inicio del programa");
+        Expendedor exp = new Expendedor(12); //Instanciamos nuestro expendedor con 12 instancias de cada producto en sus respectivos depósitos
+
+        ArrayList<Moneda> arr = new ArrayList<>(); //Creamos un arreglo de monedas para mostrar el comparable
+        arr.add(new Moneda1000());
+        arr.add(new Moneda100()); //Añadimos monedas al arreglo en forma desordenada
+        arr.add(new Moneda500());
+        arr.sort(null); //Probamos el sort (null indica que la forma de sortear será con el compareTo que definimos en la superclase Moneda)
+        for(Moneda m : arr){ //Similar a hacer un for Moneda in arr en Python
+            System.out.println(m.getValor()); //Imprimimos el valor de la moneda en orden para ver si se sorteó correctamente
+        }
     }
 }
